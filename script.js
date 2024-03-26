260f2653f7825b1852a42a66bf054afe20497b05
James <millerrj00@gmail.com>
2024-03-25 22:26:54 -0700

    styling and extra touches

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ const taskList = document.getElementById("tasklist")
 function handleSubmit(event) {
  event.preventDefault()
  let newTask = document.getElementById("newtask").value
+ if (newTask === '') {
+  alert("Enter a task")
+  return
+ }
  let listItem = document.createElement("li")
  let text = document.createTextNode(newTask)
  listItem.append(text)
  taskList.append(listItem);
  let span = document.createElement("span")
+ span.className = "removeSpan"
  let remove = document.createElement("button")
- remove.textContent = "Delete"
+ remove.className = "removeButton"
+ remove.textContent = "\u00D7"
  remove.addEventListener("click", removeItem)
  span.append(remove)
  listItem.append(span)
+ document.getElementById("newtask").value = ''
 }
-
+// delete items from todo list
 function removeItem(event) {
   let task = event.target.parentElement.parentElement
-  taskList.remove(task)
+  console.log(task)
+  taskList.removeChild(task)
 }
 
 let newTaskForm = document.getElementById("taskform")
